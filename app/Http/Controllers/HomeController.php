fb4f0537e9e073509e3196137945f56fc73ec677
JulienBailly08 <julienbailly08@gmail.com>
2022-01-19 12:56:38 +0100

    update db and pictures

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
 
-        $cars = Cars::paginate(9);
+        $cars = Cars::paginate(6);
 
         return view('home', [
             'cars' => $cars
